ba320627b2c784d5d3f42e161917a017ba83aaa2
YurikaPetrov96 <133260205+YurikaPetrov96@users.noreply.github.com>
2023-07-15 22:48:06 -0300

    cambios al orden de archivos

--- run.py
+++ run.py
@@ -6,13 +6,13 @@ import hashlib
 class use_json: #clase Json para poder tener mejor las variables a usar.
     @staticmethod
     def save_data(data):
-        with open("src/users.json", "w") as file:
+        with open("data/users.json", "w") as file:
             json.dump(data, file)
     
     @staticmethod
     def load_data():
         try:
-            with open("src/users.json", "r") as file:
+            with open("data/users.json", "r") as file:
                 return json.load(file)
         except (FileNotFoundError, json.JSONDecodeError):
             return {}
@@ -51,9 +51,9 @@ class Usuario: # clase usuario inicializada, y con metodos estaticos.
             user_data = data[username]
             hashed_password = Usuario.hash_password(password, user_data["salt"])
             if hashed_password == user_data["hashed_password"]:
-                print("Autentificacion Permitida")
+                print("Iniciaste Sesion.")
                 return True
-        print("Autentificacion Fracasada.")
+        print("No pudiste iniciar sesion.")
         return False
 
 class Main_frame(tk.Frame):
@@ -62,28 +62,47 @@ class Main_frame(tk.Frame):
     """
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
-        registro_label = ttk.Label(self, text="Registrese por favor: ")
-        registro_label.grid(row=0, column=0, padx=10, pady=10)
+        registro_label = ttk.Label(self, text="Inicie Sesion o Registrese: ")
+        registro_label.grid(row=0, column=0, padx=10, pady=1)
 
         self.username_label = ttk.Label(self, text="Ingrese su nombre de Usuario: ")
-        self.username_label.grid(row=1, column=0, padx=10, pady=10)
+        self.username_label.grid(row=1, column=0, padx=0, pady=10)
         self.username_entry = ttk.Entry(self)
-        self.username_entry.grid(row=1, column=1, padx=10, pady=10)
+        self.username_entry.grid(row=1, column=1, padx=1, pady=10)
 
         self.password_label = ttk.Label(self, text="Ingrese su contraseña: ")
-        self.password_label.grid(row=2, column=0, padx=10, pady=10)
+        self.password_label.grid(row=2, column=0, padx=0, pady=10)
         self.password_entry = ttk.Entry(self, show="*")
-        self.password_entry.grid(row=2, column=1, padx=10, pady=10)
+        self.password_entry.grid(row=2, column=1, padx=0, pady=10)
 
+        add_button = ttk.Button(self, text="Registrarse", command=Application.Change_frame)
+        add_button.grid(row=3, column=0, columnspan=2, padx=1, pady=2)
+
+        authenticate_button = ttk.Button(self, text="Autenticar", command=Usuario.autentificar_usuario)
+        authenticate_button.grid(row=3, column=1, columnspan=2, padx=1, pady=2)
+
+
+class Registro_frame(tk.Frame):
+    def __init__(self, registro, *args, **kwargs):
+        super().__init__(registro, *args, **kwargs)
+        
+        registro_label = ttk.Label(self, text="Registrese por favor: ")
+        registro_label.grid(row=0, column=0, padx=10, pady=1)
+
+        self.username_label = ttk.Label(self, text="Ingrese su nombre de Usuario: ")
+        self.username_label.grid(row=1, column=0, padx=0, pady=10)
+        self.username_entry = ttk.Entry(self)
+        self.username_entry.grid(row=1, column=1, padx=1, pady=10)
+
+        self.password_label = ttk.Label(self, text="Ingrese su contraseña: ")
+        self.password_label.grid(row=2, column=0, padx=0, pady=10)
+        self.password_entry = ttk.Entry(self, show="*")
+        self.password_entry.grid(row=2, column=1, padx=0, pady=10)
+        
+        
         add_button = ttk.Button(self, text="Registrarse", command=Usuario.registrar_usuario)
         add_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
 
-        authenticate_button = ttk.Button(self, text="Autenticar", command=Usuario.autentificar_usuario)
-        authenticate_button.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
-
-
-class Registro():
-    pass
 
 
 class Application(tk.Tk):
@@ -92,9 +111,12 @@ class Application(tk.Tk):
         super().__init__(*args, **kwargs)
         self.title("App Proyecto Cimne")
         self.geometry("800x600")
-        self.iconbitmap("src/music.ico")
+        self.iconbitmap("music.ico")
         self.frame = Main_frame(self)
         self.frame.pack()
+    
+    def Change_frame():
+        pass
 
 
 if __name__ == "__main__":
