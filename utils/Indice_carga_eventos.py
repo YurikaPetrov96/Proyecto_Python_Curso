d3016329fc482caa1b666607e5c0744b8005b181
LarranJorge <llarran52@gmail.com>
2023-07-19 18:21:43 -0300

    Carga de eventos e indice
    Script de carga de eventos y creacion del indice de eventos

---
+++ utils/Indice_carga_eventos.py
@@ -0,0 +1,47 @@
+import json
+
+class Evento:
+    def __init__(self, nombre, artista, genero, ubicacion, horario_ini, horario_fin):
+        self.nombre = nombre
+        self.artista = artista
+        self.genero = genero
+        self.ubicacion = ubicacion
+        self.horario_ini = horario_ini
+        self.horario_fin = horario_fin
+
+class Indice:
+    def __init__(self, archivo):
+        self.archivo = archivo
+        self.eventos = self.carga()
+
+    def carga(self):
+        try:
+            with open(self.archivo, 'r') as f:
+                eventos = json.load(f)
+        except FileNotFoundError:
+            eventos = []
+        return eventos
+    
+    def guardar(self):
+        with open(self.archivo, 'w') as f:
+            json.dump(self.eventos, f, indent=4)
+
+    def agregar(self, evento):
+        self.eventos.append(evento.__dict__)
+        self.guardar()
+
+    def mostrar(self):
+        if len(self.eventos) == 0:
+            print("No hay eventos registrados")
+        else:
+            print("Eventos regitrados: ")
+            for index, evento in enumerate(self.eventos, start = 1):
+                print(f"Evento {index}:")
+                print(f"Nombre: {evento['nombre']}")
+                print(f"Artista: {evento['artista']}")
+                print(f"Genero: {evento['genero']}")
+                print(f"Ubicacion: {evento['ubicacion']}")
+                print(f"Horario de inicio: {evento['horario inicio']}")
+                print(f"Horario de finalizacion: {evento['horario fin']}")
+
+indice = Indice('basededatos.json')
